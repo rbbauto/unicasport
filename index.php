c2602b796b83177177b480ee949c358c1f47b1ee
Richard Barolin - Autoradio RBB <electronicarbb@yahoo.com.ar>
2020-04-29 16:59:50 -0300

    Sitio Unica Sport v0.0.1

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Fabrica de Pelotas Unica Sport</title>
+        <!-- Favicon-->
+        <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- Third party plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container">
+                <i class="fas fa-futbol fa-lg"></i><a class="navbar-brand js-scroll-trigger" href="#page-top">&nbspUnica Sport</a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Empresa</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Servicios</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Galeria</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contacto</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end">
+                        <h1 class="text-uppercase text-white font-weight-bold">Fabrica de Pelotas y Venta de Articulos Deportivos</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-danger-75 font-weight-bolder mb-5">Fabricamos Todo tipo de Pelotas: Comerciales, Profesionales, Publicitarias y Vendemos Articulos Deportivos!</p>
+                        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Mas Informacion</a>
+                    </div>
+
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="page-section bg-info" id="about">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="text-dark mt-0">Acerca de Nosotros</h2>
+                        <p class="text-dark-50 font-weight-bolder">
+                            Somos una Empresas con mas de 38 años de Experiancias, Fabricamos Pelotas de todo tipo y uso:Comercial, Profesional, Publicitario. Nuestra Experiencia en el rubro nos avalan para dar soluciones a sus requerimientos, tenemos stock para surtir cualquier demanda y si no tenemos lo que busca nuestra capacidad de produccion lo hara!
+                        </p>
+                        <h3 class="text-dark mt-0">Fabricamos y Comercializamos</h3>
+                        <p class="text-dark-50 mb-4">Pelotas de: Futbol,Voley,Handball,Papi(1/2 pique), Basquet, Rugby, Tenis.!</p>
+                        <p class="text-dark-50 mb-4">Articulos Deportivos:Vestimenta, Rodilleras, Colchonetas, Bolsas, Guantes, Pesas y mucho mas!</p>
+                        <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Ver Mas!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container">
+                <h2 class="text-center mt-0">Nuestros Servicios</h2>
+                <hr class="divider my-4" />
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-futbol text-primary fa-5x"></i>
+                            <h3 class="h4 mb-2">Pelotas</h3>
+                            <p class="text-muted mb-0">Vulcanizadas, cosidas,para jugueteria, profesionales, publicitarias, entrenamiento y muchas mas!</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-dumbbell text-primary fa-5x"></i>
+                            <h3 class="h4 mb-2">Articulos Deportivos</h3>
+                            <p class="text-muted mb-0">Pesas, Colchonetas, Guantes, Conos de Entrenamiento, Vestimenta, Canilleras y mas!</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="far fa-lightbulb text-primary fa-5x"></i>
+                            <h3 class="h4 mb-2">Personalizadas!</h3>
+                            <p class="text-muted mb-0">Publicidad, souvenirs, con logo y mas!</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-concierge-bell text-primary fa-5x"></i>
+                            <h3 class="h4 mb-2">Atencion Personalizada</h3>
+                            <p class="text-muted mb-0">Nustro personal esta atento a sus inquietudes y requerimientos!</p>
+
+                        </div>
+
+                    </div>
+                    <div class="col-lg-12 text-center">
+                        <a class="btn btn-dark btn-xl js-scroll-trigger" href="#portfolio">Galeria</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Portfolio-->
+        <section id="portfolio">
+            <div class="container-fluid p-0">
+                <div class="row no-gutters">
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/galeria/fullsize/entrenamiento.jpg"
+                            ><img class="img-fluid" src="assets/img/galeria/thumbnails/entrenamiento.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Articulos Deportivos:</div>
+                                <div class="project-name">Para entrenamiento y mas!</div>
+                            </div></a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/galeria/fullsize/pesas.jpg"
+                            ><img class="img-fluid" src="assets/img/galeria/thumbnails/pesas.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Articulos de Gimnasio</div>
+                                <div class="project-name">Pesas, discos, barras, manuernas y mas!</div>
+                            </div></a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/galeria/fullsize/pelotas.jpg"
+                            ><img class="img-fluid" src="assets/img/galeria/thumbnails/pelotas.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Pelotas</div>
+                                <div class="project-name">Simil Cuero, Cuero, papi, handball, basket, personalizadas, publicidad y mas!</div>
+                            </div></a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/galeria/fullsize/box.jpg"
+                            ><img class="img-fluid" src="assets/img/galeria/thumbnails/box.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Articulos para entrenamiento</div>
+                                <div class="project-name">Todo para: box, artes marciales y mas!</div>
+                            </div></a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/galeria/fullsize/varios.jpg"
+                            ><img class="img-fluid" src="assets/img/galeria/thumbnails/varios.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Entrenamieto y ocio</div>
+                                <div class="project-name">Tiro al blanco, aros basket y mas!</div>
+                            </div></a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/galeria/fullsize/protFutbol.jpg"
+                            ><img class="img-fluid" src="assets/img/galeria/thumbnails/protFutbol.jpg" alt="" />
+                            <div class="portfolio-box-caption p-3">
+                                <div class="project-category text-white-50">Vestimenta y proteccion</div>
+                                <div class="project-name">Guantes, canilleras, rodilleras, shorts, remeras termicas y mas!</div>
+                            </div></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Call to action-->
+        <section class="page-section bg-dark text-white">
+            <div class="container text-center">
+                <h2 class="mb-4">Consulte a nuestros representantes!</h2>
+                <a class="btn btn-light btn-xl js-scroll-trigger" href="#contact">Contactenos</a>
+            </div>
+        </section>
+        <!-- Contact-->
+        <section class="page-section" id="contact">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="mt-0">Contactenos</h2>
+                        <hr class="divider my-4" />             
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+                        <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+                        <div>+54 9 (0237) 468-2107</div>
+                    </div>
+                    <div class="col-lg-4 mr-auto text-center">
+                       <iframe class="col-lg-12 col-md-12 col-sm-12" style="border: 0;"
+src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13132.701451211378!2d-58.7892875!3d-34.6250085!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5bf6ef4e060b472d!2sPelotas+Unica+Sport!5e0!3m2!1ses!2sar!4v1503925960380" width="300" height="350" frameborder="0" allowfullscreen="allowfullscreen">
+</iframe
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="bg-light py-5">
+            <div class="container">
+                <div class="col-md-12 text-center">
+                    <ul class="social-network social-circle">
+                        
+                        <li><a href="https://www.facebook.com/Unica-Sport-443298942525772/" class="icoFacebook" title="Facebook"><i class="fab fa-facebook fa-3x"></i></a></li>
+                        <li><a href="https://plus.google.com/113862832649718260177?rel=author" class="icoGoogle" title="Google +"><i class="fab fa-google-plus fa-3x"></i></a></li>
+                        <li><a href="tel:0237 4682107" class="icoPhone" title="llamar"><i class="fas fa-phone-square fa-3x"></i></a></li>
+                        
+                    </ul>       
+                                
+                </div>
+        
+                <div class="small text-center text-muted">Copyright © 2020 - RBB Soft</div></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
