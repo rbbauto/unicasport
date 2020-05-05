8913af212e415f161efb39e511c85541ae7caef4
Richard Barolin - Autoradio RBB <electronicarbb@yahoo.com.ar>
2020-05-05 07:56:21 -0300

    arreglos menores

--- tienda-online.php
+++ tienda-online.php
@@ -24,7 +24,7 @@
 
         <!-- Navigation-->
         
-            <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 bg-dark" id="mainNav">
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
             <div class="container">
                 <i class="fas fa-futbol fa-lg"></i><a class="navbar-brand js-scroll-trigger" href="http://<?php echo $_SERVER['SERVER_NAME'] . "/" . basename(__DIR__); ?>/">&nbspUnica Sport</a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
